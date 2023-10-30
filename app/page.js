695a3950d8ee0deb179f96c95ecd9c75028c6c11
Cho Jai Ho <128767460+datajcthemax@users.noreply.github.com>
2023-10-30 11:57:18 +0900

    get

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import { connectDB } from "/util/database.js"
 export default async function Home() {
   let db = (await connectDB).db('forum');
   let result = await db.collection('post').find().toArray();
+  console.log(result)
   return (
     <main>
       {result[0].title}
